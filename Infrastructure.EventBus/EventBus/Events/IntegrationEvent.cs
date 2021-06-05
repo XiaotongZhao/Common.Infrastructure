fbf5d9bb0bd1b9b1ccb1647f019384e05811fd50
XiaotongZhao <275451183@qq.com>
2021-06-05 14:24:33 +0800

    create project common.infrastructure

---
+++ Infrastructure.EventBus/EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Infrastructure.EventBus.EventBus.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        [Newtonsoft.Json.JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreationDate { get; private set; }
+    }
+}
